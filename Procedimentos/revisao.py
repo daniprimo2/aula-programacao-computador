2876d3089997cd90b8f991c00a117021147d2d11
daniprimo2 <daniellopes0208@hotmail.com>
2025-05-28 20:56:31 -0300

    Atividades

---
+++ Procedimentos/revisao.py
@@ -0,0 +1,70 @@
+#  Lógica e os conceitos 
+
+#  Alternativas, codigos para avaliar o funcionamento
+#
+# Estudar o print,
+# Detalhes do if e else
+# Listas
+# Detalhes do for / while
+# Indices de zero e um 
+# 
+# Lambda a + b: a + a
+# Biblioteca Match (Trigonometria)
+# como calcula ceno e coceno
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
