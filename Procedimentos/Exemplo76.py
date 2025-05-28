6d2d98b1ba5f635ee27473d7f5e3aa0a8ff8f432
daniprimo2 <daniellopes0208@hotmail.com>
2025-05-28 19:26:21 -0300

    Aulas de procedimento

---
+++ Procedimentos/Exemplo76.py
@@ -0,0 +1,11 @@
+# Exemplo 76 - exemplo de método (procedimento) por que nao retorna nada.
+
+#Sempre deve ser codificada antes da logica.
+
+
+def exibirMensagens():
+    print("Programação de computadores")
+    print("Este é um método.....")
+
+exibirMensagens()
+print("Até breve")
